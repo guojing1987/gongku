df0d3a9afce47f71d8b8d9abf638dc6473b161b4
guojing1987 <366202030@qq.com>
2026-08-24 12:36:42 +0800

    fix: bump CACHE to v4

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,5 @@
 /* Service Worker - 设备资料库 */
-const CACHE = 'gongku-v3';
+const CACHE = 'gongku-v4';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
